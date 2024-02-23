4cdc480fc0bac8979db87630c841a41183f42f35
dgumenyuk <log6305.poly@gmail.com>
2024-02-23 06:38:51 -0500

    Minor fixes

--- optimize.py
+++ optimize.py
@@ -3,12 +3,9 @@
 from datetime import datetime
 from poly_sbst.common.random_seed import get_random_seed
 from poly_sbst.common.abstract_executor import AbstractExecutor
-from poly_sbst.problems.random_problem import RandomProblem
-from poly_sbst.generators.random_generator import RandomGenerator
 from pymoo.algorithms.soo.nonconvex.ga import GA
 from pymoo.algorithms.soo.nonconvex.random_search import RandomSearch
 from poly_sbst.sampling.abstract_sampling import AbstractSampling
-from poly_sbst.mutation.random_mutation import RandomMutation
 from poly_sbst.crossover.random_crossover import OnePointCrossover
 from poly_sbst.problems.test_suite_problem import TestSuiteProblem
 from poly_sbst.generators.test_suite_generator import TestSuiteGenerator
@@ -28,22 +25,22 @@ def optimize(runs=1):
         pop_size = 10
         num_gen = 5
 
-        generator = TestSuiteGenerator() #RandomGenerator() #
+        generator = TestSuiteGenerator() 
 
-        executor = AbstractExecutor(HTMLParser().feed) #HTMLParser().feed #cgi_decode
-        problem = TestSuiteProblem(executor) #RandomProblem(executor=executor) #
+        executor = AbstractExecutor(HTMLParser().feed) 
+        problem = TestSuiteProblem(executor) 
         
         method = GA(pop_size=pop_size,
                 n_offsprings=int(pop_size/2),
-                sampling=AbstractSampling(generator),#FloatRandomSampling(),
-                mutation=TestSuiteMutation(generator=generator),#,#, (prob=0.4, eta=3.0, vtype=float) #(prob=0.4, eta=3.0, vtype=float),RandomMutation(mut_rate=0.4)
-                crossover=OnePointCrossover(cross_rate=0.9),#OnePointCrossover(cross_rate=0.9),
+                sampling=AbstractSampling(generator),
+                mutation=TestSuiteMutation(generator=generator),
+                crossover=OnePointCrossover(cross_rate=0.9),
                 eliminate_duplicates=False
                 )
         
         res = minimize(problem,
                 method,
-                termination=('n_gen', num_gen),#BeamNGTermination(),#
+                termination=('n_gen', num_gen),
                 seed=seed,
                 verbose=True,
                 eliminate_duplicates=False,
@@ -53,7 +50,6 @@ def optimize(runs=1):
         print("Best solution found: %s" % res.X)
         print("Function value: %s" % res.F)
         print("Execution data:", res.problem.execution_data)
-        #log.info(f"Executor data {executor.test_dict}")
 
 
 if __name__ == "__main__":
